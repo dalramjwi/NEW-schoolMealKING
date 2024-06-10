d3e3406c9c44730087b51656327ee7029852464a
dalramjwi <dalramjwi@gmail.com>
2024-06-10 16:58:03 +0900

    making div for condition check

---
+++ public/js_module/tableSet.js
@@ -0,0 +1,5 @@
+// import { buttonSet } from "./buttonSet.js";
+export const tableSet = function (row, col, path) {
+  for (let i = 0; i < row * col; i++) {}
+  const div = document.createElement("div");
+};
