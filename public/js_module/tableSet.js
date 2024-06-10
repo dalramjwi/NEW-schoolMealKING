d612daac7f7fdc619859cdfc8e7932d430dc0dad
dalramjwi <dalramjwi@gmail.com>
2024-06-10 18:12:48 +0900

    using my module

--- public/js_module/tableSet.js
+++ public/js_module/tableSet.js
@@ -1,5 +1,9 @@
 // import { buttonSet } from "./buttonSet.js";
-export const tableSet = function (row, col, path) {
-  for (let i = 0; i < row * col; i++) {}
-  const div = document.createElement("div");
+const tableSet = function (row, col, path) {
+  for (let i = 0; i < row * col; i++) {
+    console.log("반복");
+  }
+  // const div = document.createElement("div");
 };
+
+const test = tableSet(3, 3);
