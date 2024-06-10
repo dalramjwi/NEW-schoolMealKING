97c264f2ddb7b3e4c1ac28f010d00eab9123780d
dalramjwi <dalramjwi@gmail.com>
2024-06-10 15:51:16 +0900

    fix index css and js

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 import { formSet3 } from "./formSet3.js";
 // const button = buttonSet("submit", "진행하기");
 const root = document.getElementById("root");
-const rootText = document.getElementById("rootText");
-const formdata = ["/menu", "post", rootText];
+const rOne = document.getElementById("rOne");
+const rTwo = document.getElementById("rTwo");
+const formdata = ["/menu", "post", rTwo];
 const buttondata = ["submit", "진행하기"];
 formSet3(formdata, buttondata);
 root.addEventListener("click", () => {
-  rootText.style.display = "block";
+  rOne.style.display = "block";
+  rTwo.style.display = "block";
 });
