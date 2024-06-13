60b27bd0dce0e8bb1bee3b4980ce911504874d6d
dalramjwi <dalramjwi@gmail.com>
2024-06-13 11:18:28 +0900

    object using check

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const modules = require("./module_assemble.js");
+const m = require("./module_assemble.js");
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", function (req, res) {
-  res.sendFile(modules.path).join(__dirname, "./public/main.html");
+  res.sendFile(m.path.join(__dirname, "./public/main.html"));
 });
 app.listen(port, () => console.log(`http://localhost:${port}`));
