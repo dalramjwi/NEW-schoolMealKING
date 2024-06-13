07799a79380ebd4f4e4af418d6bcb0e92d136ad6
dalramjwi <dalramjwi@gmail.com>
2024-06-13 11:50:23 +0900

    tagIdmaker make

---
+++ src/my_module/component/tagIdMaker.js
@@ -0,0 +1,10 @@
+/**
+ * 매개변수로 만들 tag와 tag의 id를 넣어주면 tag를 만들어주는 함수이다.
+ * @param {string} tagName tag의 이름
+ * @param {string} content content 부분의 text
+ * @param {string} tagId tag의 id
+ * @returns <${tagName} id = "${tagId}">${content}</${tagName}>
+ */
+const tagMaker = function (tagName, content, tagId) {
+  return `<${tagName} id = "${tagId}">${content}</${tagName}>`;
+};
