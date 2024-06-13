ef8b6392d1d51112cc9a82bf8205efa2349ad8b3
dalramjwi <dalramjwi@gmail.com>
2024-06-13 12:03:06 +0900

    edit name and make root tag

--- src/my_module/basic_module/component_assemble.js
+++ src/my_module/basic_module/component_assemble.js
@@ -1,4 +1,5 @@
 const m = require("../../module_assemble.js");
 const componentAssemble = {
-  body: m.tagMaker("body", content),
+  body: m.tagMaker("body", bodyContent),
+  root: m.tagIdMaker("div", rootContent, "root"),
 };
