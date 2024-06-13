e2577d18e19c210a23dc6b54f2e0bbbcc7d779f0
dalramjwi <dalramjwi@gmail.com>
2024-06-13 12:28:36 +0900

    defaultparmeter make

--- src/my_module/component/tagIdMaker.js
+++ src/my_module/component/tagIdMaker.js
@@ -1,11 +1,11 @@
 /**
  * 매개변수로 만들 tag와 tag의 id를 넣어주면 tag를 만들어주는 함수이다.
  * @param {string} tagName tag의 이름
- * @param {string} content content 부분의 text
  * @param {string} tagId tag의 id
+ * @param {string} content content 부분의 text 기본 값 ""
  * @returns <${tagName} id = "${tagId}">${content}</${tagName}>
  */
-const tagIdMaker = function (tagName, content, tagId) {
+const tagIdMaker = function (tagName, tagId, content = "") {
   return `<${tagName} id = "${tagId}">${content}</${tagName}>`;
 };
 module.exports = tagIdMaker;
