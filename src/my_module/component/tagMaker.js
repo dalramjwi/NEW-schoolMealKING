24c3e42195d3910da089954fa8136da85721336e
dalramjwi <dalramjwi@gmail.com>
2024-06-13 11:48:43 +0900

    tagMaker comment

--- src/my_module/component/tagMaker.js
+++ src/my_module/component/tagMaker.js
@@ -1,4 +1,9 @@
 /**
  * 매개변수로 만들 tag를 넣어주면 tag를 만들어주는 함수이다.
+ * @param {string} tagName tag의 이름
+ * @param {string} content content 부분의 text
+ * @returns <${tagName}>${content}</${tagName}>
  */
-const tagMaker = function () {};
+const tagMaker = function (tagName, content) {
+  return `<${tagName}>${content}</${tagName}>`;
+};
