c9bb698742ce72cda7a7c56a5cf3b58e2310d4b7
dalramjwi <dalramjwi@gmail.com>
2024-06-13 11:09:20 +0900

    use module assemble

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const path = require("path");
+const { path } = require("./module_assemble.js");
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", function (req, res) {
-  res.send();
+  res.send(path);
 });
 app.listen(port, () => console.log(`http://localhost:${port}`));
