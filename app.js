ba3aabd0e8bd089af772278871f253f2cd3b3275
dalramjwi <dalramjwi@gmail.com>
2024-06-13 10:53:49 +0900

    setting app.get and app.listen

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "main.html"));
+});
+app.listen(port, () => console.log(`http://localhost:${port}`));
