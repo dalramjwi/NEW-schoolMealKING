aee3ba5e00accd2a0fbfd1c12715427cd7f152ea
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 16:07:49 +0900

    make bodyMaker function and check, not working right

--- src/my_module/component/bodyMaker.js
+++ src/my_module/component/bodyMaker.js
@@ -1,1 +1,12 @@
 const divMaker = require("./divMaker");
+/**
+ * body 태그 안에 리터럴 요소 삽입하는 함수
+ * @param {string} content body 안에 들어갈 요소
+ */
+function bodyMaker(content) {
+  return `<body>${content}</body>`;
+}
+const body = {
+  mainBody: bodyMaker(divMaker.root(divMaker.mainRootContent)),
+};
+console.log(body.mainBody);
