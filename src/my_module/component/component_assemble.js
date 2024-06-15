7906f7f9ff1430d95769cfc4f8edc1cf47aa277c
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 15:58:25 +0900

    using fix and check

--- src/my_module/component/component_assemble.js
+++ src/my_module/component/component_assemble.js
@@ -2,6 +2,6 @@ const top = require("../basic_module/literalParts/top");
 const end = require("../basic_module/literalParts/end");
 const divMaker = require("./divMaker");
 const componentAssemble = {
-  main: top + end,
+  main: top.baseTop("main", "public/css/main") + end.baseEnd("public/js/main"),
 };
-console.log(componentAssemble.mains);
+console.log(componentAssemble.main);
