2bf6729aaf6aa3d3bb619cae26508d320727237d
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 15:31:33 +0900

    에러 처리 추가

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ const port = process.env.PORT || 3000;
 app.get("/", function (req, res) {
   res.sendFile(m.path.join(__dirname, "./public/main.html"));
 });
+app.use(function (err, req, res) {
+  res.send("Error EXist");
+});
 app.listen(port, () => console.log(`http://localhost:${port}`));
