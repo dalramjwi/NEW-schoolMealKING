69ba39b72e8fc52ed98262499e65ed79bfe721fc
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 15:33:12 +0900

    module 집합 객체 일단 비움

--- src/module_assemble.js
+++ src/module_assemble.js
@@ -3,12 +3,6 @@
  */
 const modules = {
   path: require("path"),
-  tagMaker: require("./my_module/component/tagMaker"),
-  tagIdMaker: require("./my_module/component/tagIdMaker"),
-  componentWork: require("./my_module/component/componentObj"),
-  top: require("./my_module/basic_module/literalParts/top"),
-  end: require("./my_module/basic_module/literalParts/end"),
-  makeComponent: require("./my_module/component/makeComponent"),
 };
 console.log(modules);
 module.exports = modules;
