8203495b0a7fee44f98e575d40371fcdc4279969
dalramjwi <dalramjwi@gmail.com>
2024-06-15 19:21:54 +0900

    main.js base

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+//div id, 변수 할당
+const line = document.getElementById("line");
+const letter = document.getElementById("letter");
+const page = document.getElementById("page");
+const bLine = document.getElementById("bLine");
