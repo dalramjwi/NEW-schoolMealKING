8b99090080fde18a0b76e77b074910c5e8866144
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 15:49:05 +0900

    fix to divMaker

---
+++ src/my_module/component/divMaker.js
@@ -0,0 +1,16 @@
+const tagIdMaker = require("./tagIdMaker");
+const divMaker = {
+  //공통 부분
+  root: (rootContent) => {
+    return tagIdMaker("div", "root", rootContent);
+  },
+  line: tagIdMaker("div", "line"),
+  bLine: tagIdMaker("div", "bLine"),
+  //별개 HTML 부분
+  mainRootContent: () => {
+    const page = tagIdMaker("div", "page");
+    const letter = tagIdMaker("div", "letter", page);
+    return divMaker.line + letter + divMaker.bLine;
+  },
+};
+module.exports = divMaker;
