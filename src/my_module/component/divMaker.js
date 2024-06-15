69ea0c91aa97d63b5bcd561d394a5db609dfba69
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 16:03:36 +0900

    add comment

--- src/my_module/component/divMaker.js
+++ src/my_module/component/divMaker.js
@@ -1,12 +1,19 @@
 const tagIdMaker = require("./tagIdMaker");
 const divMaker = {
   //공통 부분
+  /**
+   * root Div 만드는 함수
+   * @param {function} rootContent root의 안에 들어갈 요소
+   */
   root: (rootContent) => {
     return tagIdMaker("div", "root", rootContent);
   },
   line: tagIdMaker("div", "line"),
   bLine: tagIdMaker("div", "bLine"),
   //별개 HTML 부분
+  /**
+   * main의 root에 들어갈 내용의 함수
+   */
   mainRootContent: () => {
     const page = tagIdMaker("div", "page");
     const letter = tagIdMaker("div", "letter", page);
