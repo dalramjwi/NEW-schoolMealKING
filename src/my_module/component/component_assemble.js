eb0a489748a53de9152deb0fdb544401e4d83baf
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 16:01:53 +0900

    make mainBody but not working

--- src/my_module/component/component_assemble.js
+++ src/my_module/component/component_assemble.js
@@ -2,6 +2,10 @@ const top = require("../basic_module/literalParts/top");
 const end = require("../basic_module/literalParts/end");
 const divMaker = require("./divMaker");
 const componentAssemble = {
-  main: top.baseTop("main", "public/css/main") + end.baseEnd("public/js/main"),
+  mainBody: divMaker.root(divMaker.mainRootContent()),
+  main:
+    top.baseTop("main", "public/css/main") +
+    componentAssemble.mainBody +
+    end.baseEnd("public/js/main"),
 };
 console.log(componentAssemble.main);
