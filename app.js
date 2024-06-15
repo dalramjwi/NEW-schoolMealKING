edc37c08eee0c57c0e3d6ba8aec755a7b1ebf88b
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-15 16:27:28 +0900

    css and js not working, using static solve matter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const m = require("./src/module_assemble.js");
 const app = express();
 const port = process.env.PORT || 3000;
+app.use("/public", express.static("public"));
 app.get("/", function (req, res) {
   res.send(m.componentAssemble.main);
 });
