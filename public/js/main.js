a99d884f2e5c74f6555d5f993a464634ec60bb34
dalramjwi <dalramjwi@gmail.com>
2024-06-15 21:40:02 +0900

    formSet use and using boolean make tag once

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,22 @@
 //모듈 호출
 import { tagIdMaker } from "../js_module/tagIdComponent.js";
+import { btnFormSet } from "../js_module/btnFormSet.js";
 //div id, 변수 할당
 const line = document.getElementById("line");
 const letter = document.getElementById("letter");
 const page = document.getElementById("page");
 const bLine = document.getElementById("bLine");
+
+let formData = ["./menu", "POST", bLine];
+let buttonData = ["submit", "진행하기"];
+
+let makingTag = true;
 letter.addEventListener("click", () => {
-  page.innerHTML = tagIdMaker("div", "text", "내용");
-  bLine.innerHTML = tagIdMaker("button", "btn", "다음");
+  if (makingTag) {
+    page.innerHTML = tagIdMaker("div", "text", "내용");
+    btnFormSet(formData, buttonData);
+    makingTag = false;
+  } else {
+    console.log("already make tag");
+  }
 });
