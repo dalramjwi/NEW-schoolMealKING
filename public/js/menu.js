9e4cd3091e0cbbcd736ab1b8d34b0de1abd508d1
dalramjwi <dalramjwi@gmail.com>
2024-06-16 13:49:20 +0900

    gridMake is working good

--- public/js/menu.js
+++ public/js/menu.js
@@ -12,10 +12,13 @@ let formData = ["./cafe", "POST", bLine];
 let buttonData = ["submit", "진행하기"];
 const divArr = [];
 function gridMake(row, col) {
-  // for (let i = 0; i < row * col; i++) {
-  //   divArr.push(tagIdMaker("div", i, "내용"));
-  // }
-  // return divArr;
-  let gridDiv = divArr.map((item) => {});
+  for (let i = 0; i < row * col; i++) {
+    divArr.push(tagIdMaker("div", i, "내용"));
+  }
+  return divArr;
 }
-console.log(gridMake(4, 7));
+gridMake(4, 7);
+const app = () => {
+  day.innerHTML = divArr.join("");
+};
+app();
