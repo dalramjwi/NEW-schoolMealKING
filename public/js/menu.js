29e9dc771601401ce1f86b600df07c50d52496e2
dalramjwi <dalramjwi@gmail.com>
2024-06-16 18:07:05 +0900

    comment

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,6 +2,7 @@
 import { tagIdMaker } from "../js_module/tagIdComponent.js";
 import { btnFormSet } from "../js_module/btnFormSet.js";
 import { gridTextSet } from "../js_module/gridTextSet.js";
+import { checkboxEvent, updateMenu } from "./../js_module/checkboxEvent.js";
 //div id, 변수 할당
 const line = document.getElementById("line");
 const graph = document.getElementById("graph");
@@ -29,45 +30,9 @@ const app = () => {
   menu.innerHTML = menuName.join("") + gridTextSet.menumap.join("");
 };
 app();
-// 선택된 메뉴를 담을 배열
-let selectedMenus = [];
-// 체크박스 클릭 이벤트
+//checkbox Event 실행
 menu.addEventListener("click", (event) => {
-  if (event.target.type === "checkbox") {
-    const checkbox = event.target;
-    if (checkbox.checked) {
-      selectedMenus.push(checkbox.value);
-      if (selectedMenus.length > 3) {
-        checkbox.checked = false;
-        selectedMenus = selectedMenus.filter((item) => item !== checkbox.value);
-        alert("최대 3개까지만 선택할 수 있습니다.");
-        return;
-      }
-    } else {
-      selectedMenus = selectedMenus.filter((item) => item !== checkbox.value);
-    }
-    console.log("선택된 메뉴 : ", selectedMenus);
+  checkboxEvent(event, () => {
     updateMenu(7);
-  }
+  });
 });
-//menu 배열을 반영하는 이벤트 함수
-function updateMenu(col) {
-  const title = document.title;
-  const titleNum = parseInt(title.replace(/\D/g, ""));
-  console.log(titleNum);
-  if (titleNum) {
-    const divs = [titleNum + col, titleNum + col * 2, titleNum + col * 3];
-    const divOne = document.getElementById(`${divs[0]}`);
-    const divTwo = document.getElementById(`${divs[1]}`);
-    const divThree = document.getElementById(`${divs[2]}`);
-    if (divOne) {
-      divOne.innerHTML = selectedMenus[0] || "";
-      if (divTwo) {
-        divTwo.innerHTML = selectedMenus[1] || "";
-        if (divThree) {
-          divThree.innerHTML = selectedMenus[2] || "";
-        }
-      }
-    }
-  }
-}
---
+++ public/js_module/checkboxEvent.js
@@ -0,0 +1,45 @@
+// 선택된 메뉴를 담을 배열
+let selectedMenus = [];
+// 체크박스 클릭 이벤트
+export function checkboxEvent(event) {
+  if (event.target.type === "checkbox") {
+    const checkbox = event.target;
+    if (checkbox.checked) {
+      selectedMenus.push(checkbox.value);
+      if (selectedMenus.length > 3) {
+        checkbox.checked = false;
+        selectedMenus = selectedMenus.filter((item) => item !== checkbox.value);
+        alert("최대 3개까지만 선택할 수 있습니다.");
+        return;
+      }
+    } else {
+      selectedMenus = selectedMenus.filter((item) => item !== checkbox.value);
+    }
+    console.log("선택된 메뉴 : ", selectedMenus);
+    updateMenu(7);
+  }
+}
+//menu 배열을 반영하는 이벤트 함수
+/**
+ * @param {number} col grid col
+ */
+export function updateMenu(col) {
+  const title = document.title;
+  const titleNum = parseInt(title.replace(/\D/g, ""));
+  console.log(titleNum);
+  if (titleNum) {
+    const divs = [titleNum + col, titleNum + col * 2, titleNum + col * 3];
+    const divOne = document.getElementById(`${divs[0]}`);
+    const divTwo = document.getElementById(`${divs[1]}`);
+    const divThree = document.getElementById(`${divs[2]}`);
+    if (divOne) {
+      divOne.innerHTML = selectedMenus[0] || "";
+      if (divTwo) {
+        divTwo.innerHTML = selectedMenus[1] || "";
+        if (divThree) {
+          divThree.innerHTML = selectedMenus[2] || "";
+        }
+      }
+    }
+  }
+}
