a23ab85e8ed4f7b776536bd110986ec234cbe851
dalramjwi <dalramjwi@gmail.com>
2024-06-16 13:28:16 +0900

    base js set and make button template base

---
+++ public/js/menu.js
@@ -0,0 +1,12 @@
+//모듈 호출
+import { tagIdMaker } from "../js_module/tagIdComponent.js";
+import { btnFormSet } from "../js_module/btnFormSet.js";
+//div id, 변수 할당
+const line = document.getElementById("line");
+const graph = document.getElementById("graph");
+const day = document.getElementById("day");
+const menu = document.getElementById("menu");
+const bLine = document.getElementById("bLine");
+
+let formData = ["./cafe", "POST", bLine];
+let buttonData = ["submit", "진행하기"];
