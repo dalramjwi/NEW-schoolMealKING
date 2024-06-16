478eb70fd4fcbf5b58ab09dd413866e80c20d4e2
dalramjwi <dalramjwi@gmail.com>
2024-06-16 13:59:28 +0900

    add comment

--- public/js/menu.js
+++ public/js/menu.js
@@ -8,10 +8,12 @@ const graph = document.getElementById("graph");
 const day = document.getElementById("day");
 const menu = document.getElementById("menu");
 const bLine = document.getElementById("bLine");
-
+//기본 실행 DOMAPI 조작
+//btn 만들기
 let formData = ["./cafe", "POST", bLine];
 let buttonData = ["submit", "진행하기"];
-
+btnFormSet(formData, buttonData);
+//day와 menu에 div 만들기
 const app = () => {
   day.innerHTML = gridSet(4, 7, "내용1").join("");
   menu.innerHTML = gridSet(6, 3, "내용2").join("");
