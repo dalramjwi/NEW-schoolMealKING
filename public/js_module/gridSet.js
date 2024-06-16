e6c0c64f139d173c3060733f7d7fe93d1f352129
dalramjwi <dalramjwi@gmail.com>
2024-06-16 13:57:27 +0900

    fix arr position

--- public/js_module/gridSet.js
+++ public/js_module/gridSet.js
@@ -1,6 +1,5 @@
 import { tagIdMaker } from "../js_module/tagIdComponent.js";
 
-const divArr = [];
 /**
  * @param {number} row grid row 해당하는 숫자
  * @param {number} col grid col 해당하는 숫자
@@ -8,6 +7,7 @@ const divArr = [];
  * @returns grid의 row, col 만큼의 div와 div 안의 text를 만들어준다.
  */
 export function gridSet(row, col, content = "") {
+  const divArr = [];
   for (let i = 0; i < row * col; i++) {
     divArr.push(tagIdMaker("div", i, content));
   }
