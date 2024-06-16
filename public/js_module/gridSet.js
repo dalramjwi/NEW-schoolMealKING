5e85fa3fc96e20b0c6081bed7e3a713e32b071ec
dalramjwi <dalramjwi@gmail.com>
2024-06-16 13:52:18 +0900

    gridMake module

---
+++ public/js_module/gridSet.js
@@ -0,0 +1,13 @@
+const divArr = [];
+/**
+ * @param {number} row grid row 해당하는 숫자
+ * @param {number} col grid col 해당하는 숫자
+ * @param {string} content div 안의 text
+ * @returns grid의 row, col 만큼의 div와 div 안의 text를 만들어준다.
+ */
+export function gridSet(row, col, content = "") {
+  for (let i = 0; i < row * col; i++) {
+    divArr.push(tagIdMaker("div", i, content));
+  }
+  return divArr;
+}
