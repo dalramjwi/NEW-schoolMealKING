f55ccd14f8a70a5be4b659a497dccfe0a9c2e4da
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-29 16:04:30 +0900

    호감도와 영양도 포인트가 할당된 배열 복사 완료

--- public/data/menuData.js
+++ public/data/menuData.js
@@ -34,4 +34,23 @@ export const menuData = {
     "spage",
     "ice",
   ],
+  menuValue: () => {
+    // 각 메뉴 항목의 호감도 포인트와 영양도 포인트
+    const hpoint = [0, 1, 2, 1, 3, 1, 2, 2, 1, 1, 2, 2, 3];
+    const ypoint = [2, 1, 0, 0, 3, 1, 0, 0, 0, 0, 1, 1, 0];
+
+    // this.menu를 참조하여 변형
+    const menuArr = Object.keys(this.menu).reduce((acc, key) => {
+      // 카테고리의 각 메뉴 항목을 변형하여 acc 배열에 추가
+      this.menu[key].forEach((item, index) => {
+        // 메뉴 항목의 hpoint와 ypoint를 배열에서 가져옴
+        const h = hpoint[index];
+        const y = ypoint[index];
+        acc.push([item, h, y]);
+      });
+
+      return acc;
+    }, []);
+    return menuArr;
+  },
 };
