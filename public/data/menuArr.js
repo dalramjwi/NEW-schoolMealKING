bb5bba8e8316665bee5baeb4bd1d9e3f99d3eb41
dalramjwi <dalramjwi@gmail.com>
2024-06-29 21:54:06 +0900

    모듈 둘 다 사용하려 했으나, 방법이 너무 좋지 않다 생각해 차선책 선택

--- public/data/menuArr.js
+++ public/data/menuArr.js
@@ -1,39 +1,9 @@
-export const menuData = {
-  /**
-   * @param {number} col grid의 col 수
-   * @param {number} month 해당 설정할 달
-   * @returns 첫 칸은 비어있는 날짜 리터럴 배열
-   */
-  date: (col, month) => {
-    const date = [""];
-    for (let i = 1; i < col; i++) {
-      date.push(`${month}.${i}`);
-    }
-    return date;
-  },
+const menuArr = {
   menu: {
     han: ["고등어 구이", "김치찌개", "불고기", "잡채", "볶음밥"],
     yang: ["샐러드", "스프", "피자", "햄버거", "스파게티"],
     hoo: ["떡", "마늘빵", "주스", "쿠키", "아이스크림"],
   },
-
-  idArr: [
-    "fish",
-    "salad",
-    "riceCake",
-    "kimchi",
-    "soup",
-    "bread",
-    "gogi",
-    "pizza",
-    "juice",
-    "jap",
-    "ham",
-    "cookie",
-    "bob",
-    "spage",
-    "ice",
-  ],
   menuValue: () => {
     // 각 메뉴 항목의 호감도 포인트와 영양도 포인트
     const hpoint = [0, 1, 2, 1, 3, 1, 2, 2, 1, 1, 2, 2, 3];
@@ -54,3 +24,4 @@ export const menuData = {
     return menuArr;
   },
 };
+module.exports = menuArr;
