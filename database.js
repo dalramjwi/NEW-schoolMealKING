78c579ae4c2fdb8fe8d0375aecde98481800dfa7
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-29 14:40:41 +0900

    making db func

--- database.js
+++ database.js
@@ -3,16 +3,18 @@ const db = new database.Database("./database/base.db", (err) => {
   console.log("에러 발생 : ", err);
 });
 const createDb = (tableName, rowOne, rowTwo, rowThree) => {
-  return db.run(
-    `CREATE TABLE ${tableName} (${rowOne} TEXT NOT NULL, ${rowTwo} NUMBER NOT NULL, ${rowThree} NUMBER NOT NULL )`,
-    (err) => {
-      if (err) {
-        console.log("오류 : ", err);
-      } else {
-        console.log(`${tableName} 생성됨`);
+  return () => {
+    db.run(
+      `CREATE TABLE ${tableName} (${rowOne} TEXT NOT NULL, ${rowTwo} NUMBER NOT NULL, ${rowThree} NUMBER NOT NULL )`,
+      (err) => {
+        if (err) {
+          console.log("오류 : ", err);
+        } else {
+          console.log(`${tableName} 생성됨`);
+        }
       }
-    }
-  );
+    );
+  };
 };
 
 const baseCreate = createDb("base", "name", "hpoint", "ypoint");
