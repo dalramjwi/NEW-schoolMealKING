82cbfeffe651bd7ca49ff2a6d33f9308ce114023
dalramjwi <dalramjwi@gmail.com>
2024-06-30 15:48:51 +0900

    0과 1 둘 다 제작

--- src/my_module/component/component_assemble.js
+++ src/my_module/component/component_assemble.js
@@ -10,5 +10,13 @@ const componentAssemble = {
     top.baseTop("menu1", "public/css/menu") +
     body.menuBody +
     end.baseEnd("public/js/menu"),
+  cafe0:
+    top.baseTop("cafe0", "public/css/cafe") +
+    body.cafeBody +
+    end.baseEnd("public/js/cafe"),
+  cafe1:
+    top.baseTop("cafe1", "public/css/cafe") +
+    body.cafeBody +
+    end.baseEnd("public/js/cafe"),
 };
 module.exports = componentAssemble;
