5448bf14cc40b8d9661664e63fc3ab45b57c4d59
dalramjwi <dalramjwi@gmail.com>
2024-06-30 16:04:04 +0900

    state add

--- src/my_module/db_module/createDb.js
+++ src/my_module/db_module/createDb.js
@@ -1,6 +1,19 @@
 // 테이블을 생성하는 함수, Promise 기반
-const createDb = (db, tableName, rowOne, rowTwo, rowThree) => {
+const createDb = (db, tableName, rowOne, rowTwo, rowThree, sum = "0") => {
   return new Promise((resolve, reject) => {
+    if (sum === "1") {
+      db.run(
+        `CREATE TABLE IF NOT EXISTS ${tableName} (${rowOne} NUMBER, ${rowTwo} NUMBER)`,
+        (err) => {
+          if (err) {
+            reject(err); // 오류 발생 시 Promise를 reject
+          } else {
+            console.log(`${tableName} 생성됨`);
+            resolve(); // 성공 시 Promise를 resolve
+          }
+        }
+      );
+    }
     db.run(
       `CREATE TABLE IF NOT EXISTS ${tableName} (${rowOne} TEXT NOT NULL, ${rowTwo} NUMBER NOT NULL, ${rowThree} NUMBER NOT NULL )`,
       (err) => {
