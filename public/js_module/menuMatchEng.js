c4d063e24574d892143f05f2cdad16cae37555a2
dalramjwi <dalramjwi@gmail.com>
2024-07-10 00:39:30 +0900

    combine arr make

--- public/js_module/menuMatchEng.js
+++ public/js_module/menuMatchEng.js
@@ -1,3 +1,7 @@
 import { menuData } from "../data/menuData";
 const menuArray = [].concat(...Object.values(menuData.menu));
 const menuEngArray = [].concat(...Object.values(menuData.menuEng));
+const combinedArray = menuArray.map((item, index) => [
+  item,
+  menuEngArray[index],
+]);
