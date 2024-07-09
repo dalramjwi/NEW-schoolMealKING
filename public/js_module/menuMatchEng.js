282728bd237d59c6407326b6dea0d164ad38598c
dalramjwi <dalramjwi@gmail.com>
2024-07-10 00:22:51 +0900

    menuArr concat use:

--- public/js_module/menuMatchEng.js
+++ public/js_module/menuMatchEng.js
@@ -1,5 +1,2 @@
 import { menuData } from "../data/menuData";
-const menuArray = Object.values(menuData.menu).reduce((acc, category) => {
-  return acc.concat(category);
-}, []);
-console.log(menuArray);
+const menuArray = [].concat(...Object.values(menuData.menu));
