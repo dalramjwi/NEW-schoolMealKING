668dc2f3b0860ea605b34fd3166f1c7a165c7784
dalramjwi <dalramjwi@gmail.com>
2024-07-09 23:24:41 +0900

    작동 확인

--- public/js/cafe.js
+++ public/js/cafe.js
@@ -1,4 +1,5 @@
 import { generateImgSrc } from "../js_module/generateImgSrc.js";
+import { createImgData } from "../js_module/createImgData.js";
 import { createAndAppendImg } from "../js_module/createAppendImg.js";
 // div id, 변수 할당
 const root = document.getElementById("root");
@@ -12,16 +13,6 @@ const mealThree = document.getElementById("mealThree");
 const mealFour = document.getElementById("mealFour");
 const mealFive = document.getElementById("mealFive");
 //기본 img 설정
-const humanBaseImgSrc = generateImgSrc("human")[0];
-const humanImg = document.createElement("img");
-humanImg.src = humanBaseImgSrc;
-humanImg.alt = "Human Base Image";
-human.appendChild(humanImg);
-const foodBaseImg = generateImgSrc("food");
-const foodImg = document.createElement("img");
-foodImg.src = foodBaseImg;
-food.appendChild(foodImg);
-const showBaseImg = generateImgSrc("show");
-const showImg = document.createElement("img");
-showImg.src = showBaseImg;
-show.appendChild(showImg);
+createImgData().forEach(({ src, parent, alt }) => {
+  createAndAppendImg(src, parent, alt);
+});
