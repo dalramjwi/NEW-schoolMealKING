3c45c5c8782c5b4e8dca5aa5bdc61f6dd0b2371e
dalramjwi <dalramjwi@gmail.com>
2024-07-09 21:47:51 +0900

    make imgSrc arr

---
+++ public/js/cafe.js
@@ -0,0 +1,63 @@
+class ImgSource {
+  constructor(src) {
+    this.src = src;
+  }
+}
+imgSrc = [
+  ["show", "dayOne"],
+  ["human", { base: "base", happy: "happy", sad: "sad" }],
+  ["food", "sik"],
+  [
+    "mealOne",
+    { rice: "rice", gook: "gook", soup: "soup", spa: "spa", pizza: "pizza" },
+  ],
+  [
+    "mealTwo",
+    { rice: "rice", gook: "gook", soup: "soup", spa: "spa", pizza: "pizza" },
+  ],
+  [
+    "mealThree",
+    {
+      japchae: "japchae",
+      bool: "bool",
+      fish: "fish",
+      ddok: "ddok",
+      cookie: "cookie",
+      ham: "ham",
+      salad: "salad",
+      bread: "bread",
+      tea: "tea",
+      ice: "ice",
+    },
+  ],
+  [
+    "mealFour",
+    {
+      japchae: "japchae",
+      bool: "bool",
+      fish: "fish",
+      ddok: "ddok",
+      cookie: "cookie",
+      ham: "ham",
+      salad: "salad",
+      bread: "bread",
+      tea: "tea",
+      ice: "ice",
+    },
+  ],
+  [
+    "mealFive",
+    {
+      japchae: "japchae",
+      bool: "bool",
+      fish: "fish",
+      ddok: "ddok",
+      cookie: "cookie",
+      ham: "ham",
+      salad: "salad",
+      bread: "bread",
+      tea: "tea",
+      ice: "ice",
+    },
+  ],
+];
