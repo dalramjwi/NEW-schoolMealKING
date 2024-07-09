89a00090e904a52056c7b0fb7245d639b6aa607b
dalramjwi <dalramjwi@gmail.com>
2024-07-09 22:10:41 +0900

    imgSrc data 폴더로 이동

--- public/js/cafe.js
+++ public/js/cafe.js
@@ -16,63 +16,6 @@ class DataConverter {
     });
   }
 }
-imgSrc = [
-  ["show", "dayOne"],
-  ["human", { base: "base", happy: "happy", sad: "sad" }],
-  ["food", "sik"],
-  [
-    "mealOne",
-    { rice: "rice", gook: "gook", soup: "soup", spa: "spa", pizza: "pizza" },
-  ],
-  [
-    "mealTwo",
-    { rice: "rice", gook: "gook", soup: "soup", spa: "spa", pizza: "pizza" },
-  ],
-  [
-    "mealThree",
-    {
-      japchae: "japchae",
-      bool: "bool",
-      fish: "fish",
-      ddok: "ddok",
-      cookie: "cookie",
-      ham: "ham",
-      salad: "salad",
-      bread: "bread",
-      tea: "tea",
-      ice: "ice",
-    },
-  ],
-  [
-    "mealFour",
-    {
-      japchae: "japchae",
-      bool: "bool",
-      fish: "fish",
-      ddok: "ddok",
-      cookie: "cookie",
-      ham: "ham",
-      salad: "salad",
-      bread: "bread",
-      tea: "tea",
-      ice: "ice",
-    },
-  ],
-  [
-    "mealFive",
-    {
-      japchae: "japchae",
-      bool: "bool",
-      fish: "fish",
-      ddok: "ddok",
-      cookie: "cookie",
-      ham: "ham",
-      salad: "salad",
-      bread: "bread",
-      tea: "tea",
-      ice: "ice",
-    },
-  ],
-];
+
 const converter = new DataConverter(imgSrc);
 console.log(converter.convertedData);
---
+++ src/data/imgSrc.js
@@ -0,0 +1,58 @@
+export const imgSrc = [
+  ["show", "dayOne"],
+  ["human", { base: "base", happy: "happy", sad: "sad" }],
+  ["food", "sik"],
+  [
+    "mealOne",
+    { rice: "rice", gook: "gook", soup: "soup", spa: "spa", pizza: "pizza" },
+  ],
+  [
+    "mealTwo",
+    { rice: "rice", gook: "gook", soup: "soup", spa: "spa", pizza: "pizza" },
+  ],
+  [
+    "mealThree",
+    {
+      japchae: "japchae",
+      bool: "bool",
+      fish: "fish",
+      ddok: "ddok",
+      cookie: "cookie",
+      ham: "ham",
+      salad: "salad",
+      bread: "bread",
+      tea: "tea",
+      ice: "ice",
+    },
+  ],
+  [
+    "mealFour",
+    {
+      japchae: "japchae",
+      bool: "bool",
+      fish: "fish",
+      ddok: "ddok",
+      cookie: "cookie",
+      ham: "ham",
+      salad: "salad",
+      bread: "bread",
+      tea: "tea",
+      ice: "ice",
+    },
+  ],
+  [
+    "mealFive",
+    {
+      japchae: "japchae",
+      bool: "bool",
+      fish: "fish",
+      ddok: "ddok",
+      cookie: "cookie",
+      ham: "ham",
+      salad: "salad",
+      bread: "bread",
+      tea: "tea",
+      ice: "ice",
+    },
+  ],
+];
