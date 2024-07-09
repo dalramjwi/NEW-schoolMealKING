cd81609f87284a8134e9235b9837c8a5dfbd1780
dalramjwi <dalramjwi@gmail.com>
2024-07-09 23:07:47 +0900

    generateImgSrc 모듈화

--- public/js/cafe.js
+++ public/js/cafe.js
@@ -1,5 +1,6 @@
 import { imgSrc } from "../data/imgSrc.js";
 import { DataConverter } from "../js_module/DataConverter.js";
+import { generateImgSrc } from "../js_module/generateImgSrc.js";
 // div id, 변수 할당
 const root = document.getElementById("root");
 const line = document.getElementById("line");
@@ -12,26 +13,9 @@ const mealThree = document.getElementById("mealThree");
 const mealFour = document.getElementById("mealFour");
 const mealFive = document.getElementById("mealFive");
 
-const converter = new DataConverter(imgSrc);
 //converter는 div id에 해당하는 img 제목을 가진 객체
-console.log(converter);
-// generateImgSrc 함수 정의
-function generateImgSrc(divId) {
-  if (converter.convertedData[divId]) {
-    const value = converter.convertedData[divId];
-    if (typeof value === "object") {
-      const imgSrcPaths = [];
-      for (let key in value) {
-        if (value.hasOwnProperty(key)) {
-          imgSrcPaths.push(`./public/img/${value[key]}.png`);
-        }
-      }
-      return imgSrcPaths;
-    } else if (typeof value === "string") {
-      return [`./public/img/${value}.png`];
-    }
-  }
-}
+const converter = new DataConverter(imgSrc);
+
 console.log(generateImgSrc("mealFive"));
 
 const humanBaseImgSrc = generateImgSrc("human")[0];
---
+++ public/js_module/generateImgSrc.js
@@ -0,0 +1,17 @@
+// generateImgSrc 함수 정의
+export function generateImgSrc(divId) {
+  if (converter.convertedData[divId]) {
+    const value = converter.convertedData[divId];
+    if (typeof value === "object") {
+      const imgSrcPaths = [];
+      for (let key in value) {
+        if (value.hasOwnProperty(key)) {
+          imgSrcPaths.push(`./public/img/${value[key]}.png`);
+        }
+      }
+      return imgSrcPaths;
+    } else if (typeof value === "string") {
+      return [`./public/img/${value}.png`];
+    }
+  }
+}
