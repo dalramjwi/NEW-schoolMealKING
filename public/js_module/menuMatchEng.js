0c783e892f5e39eaf8675d6586b0b5b268de7f17
dalramjwi <dalramjwi@gmail.com>
2024-07-13 23:55:52 +0900

    잘못 import 된 경로 수정

--- public/js_module/menuMatchEng.js
+++ public/js_module/menuMatchEng.js
@@ -1,4 +1,4 @@
-import { menuData } from "../data/menuData";
+import { menuData } from "../data/menuData.js";
 const menuArray = [].concat(...Object.values(menuData.menu));
 const menuEngArray = [].concat(...Object.values(menuData.menuEng));
 export const combinedArray = menuArray.map((item, index) => [
