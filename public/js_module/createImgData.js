e804e3ac2d06e833570d32883394c94828444485
dalramjwi <dalramjwi@gmail.com>
2024-07-14 23:42:16 +0900

    반환 값 설명

--- public/js_module/createImgData.js
+++ public/js_module/createImgData.js
@@ -1,6 +1,7 @@
 import { generateImgSrc } from "../js_module/generateImgSrc.js";
 
 export function createImgData() {
+  //이미지 데이터 배열 반환
   return [
     { src: generateImgSrc("human")[0], parent: human, alt: "Human Base Image" },
     { src: generateImgSrc("food"), parent: food, alt: "sik Image" },
