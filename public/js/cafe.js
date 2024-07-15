69df4b0a3568f2004e851354f5d4f902747792dd
dalramjwi <dalramjwi@gmail.com>
2024-07-15 19:57:08 +0900

    apendImg 로직 고민중.

--- public/js/cafe.js
+++ public/js/cafe.js
@@ -76,16 +76,10 @@ function appendImg(menuName) {
   const engName = generateImgSrcByMenuName(menuName);
   //배열 메서드를 통해 engName이 포함되어 있는 객체의 key 값과 해당 engName 반환, 또한 객체의 key값을 배열에서 제거한다.
   for (let i = 0; i < foodImgArr.length; i++) {
-    foodImgArr[i].forEach((key) => {
-      console.log(foodImgArr[i]);
-      console.log(key);
-      console.log(engName);
-      if (key === engName) {
-        console.log("안녕");
-        return [key, foodImgArr[1]];
-      }
-    });
+    if (Object.keys(foodImgArr[i][1]) === engName) console.log([i]);
+    console.log("안녕");
+    return i;
   }
 }
-console.log(appendImg("떡"));
 console.log(foodImgArr);
+console.log(appendImg("떡"));
