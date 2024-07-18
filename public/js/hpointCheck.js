50690029316152d1339bdb9fcfbdd4261bff10b1
dalramjwi <dalramjwi@gmail.com>
2024-07-18 09:45:46 +0900

    text css 및 삽입

--- public/js/hpointCheck.js
+++ public/js/hpointCheck.js
@@ -1,6 +1,8 @@
 import { createAndAppendImg } from "../js_module/createAppendImg.js";
 import { generateImgSrc } from "../js_module/generateImgSrc.js";
 const root = document.getElementById("root");
+const textOne = document.getElementById("textOne");
+const textTwo = document.getElementById("textTwo");
 function createImgData() {
   return [
     {
@@ -15,3 +17,18 @@ function createImgData() {
 createImgData().forEach(({ src, parent, alt }) => {
   createAndAppendImg(src, parent, alt);
 });
+const texts = {
+  positive: ["내일 뭐 나온대?", "요즘 급식 괜찮지 않냐?"],
+  negative: ["급식 맛없어", "영양사 언제 바뀌냐"],
+};
+const title = document.title;
+if (title.includes("0")) {
+  textOne.innerText = texts.negative[0];
+  textTwo.innerText = texts.negative[1];
+} else if (title.includes("1")) {
+  textOne.innerText = texts.positive[0];
+  textTwo.innerText = texts.positive[1];
+}
+setTimeout(() => {
+  window.location.href = "/menu";
+}, 5000); // 5초 후에 실행
