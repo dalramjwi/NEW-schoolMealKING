3072e52215704041dad876b9515e71d2e93adf9e
dalramjwi <dalramjwi@gmail.com>
2024-08-29 18:01:03 +0900

    라우터 menu 주석 추가

--- src/my_module/server_module/route_menu.ts
+++ src/my_module/server_module/route_menu.ts
@@ -3,10 +3,26 @@ import m from "../../module_assemble.js";
 
 const router = express.Router();
 
+/**
+ * GET 요청에 대해 메뉴 컴포넌트를 응답으로 보냄
+ *
+ * @name get/menu
+ * @memberof module:router
+ * @param {Object} req - 요청 객체
+ * @param {Object} res - 응답 객체
+ */
 router.get("/menu", (req, res) => {
   res.send(m.componentAssemble.menu);
 });
 
+/**
+ * POST 요청에 대해 메뉴 컴포넌트를 응답으로 보냄
+ *
+ * @name post/menu
+ * @memberof module:router
+ * @param {Object} req - 요청 객체
+ * @param {Object} res - 응답 객체
+ */
 router.post("/menu", (req, res) => {
   res.send(m.componentAssemble.menu);
 });
