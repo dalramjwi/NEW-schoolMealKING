75a726dd0ec5fa1d8787bd076bd40e91f5932e5f
dalramjwi <dalramjwi@gmail.com>
2024-08-30 00:48:28 +0900

    random 모듈로 분리

---
+++ src/my_module/server_module/route_actions/random_page.js
@@ -0,0 +1,12 @@
+/**
+ * 랜덤 페이지 처리 (추가 로직 필요)
+ * @param {Object} res - 응답 객체
+ */
+function randomPage(res) {
+  res.json({
+    success: true,
+    message: "Random page processing not implemented",
+  });
+}
+
+module.exports = randomPage;
