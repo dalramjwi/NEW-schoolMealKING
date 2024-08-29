abe28ee2df7df9cec84430ea66d42347b58610cd
dalramjwi <dalramjwi@gmail.com>
2024-08-29 18:09:21 +0900

    파일 자체 설명 추가

--- src/my_module/server_module/route_menu.ts
+++ src/my_module/server_module/route_menu.ts
@@ -1,3 +1,7 @@
+/**
+ * 이 파일은 Express 라우터를 정의해서 메뉴와 관련된 GET 및 POST 요청을 처리하는 역할
+ * 클라이언트가 특정 엔드포인트로 요청을 보내면, 해당 요청에 맞는 메뉴 컴포넌트를 응답한다.
+ */
 import express from "express";
 import m from "../../module_assemble.js";
 
