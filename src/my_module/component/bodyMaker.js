97c1ba6115443274ced14cf333239238d07706f2
dalramjwi <dalramjwi@gmail.com>
2024-08-29 17:15:03 +0900

    body 제작

--- src/my_module/component/bodyMaker.js
+++ src/my_module/component/bodyMaker.js
@@ -11,5 +11,8 @@ const body = {
   menuBody: bodyMaker(divMaker.root(divMaker.menuRootContent())),
   cafeBody: bodyMaker(divMaker.root(divMaker.cafeRootContent())),
   hpoinCheckBody: bodyMaker(divMaker.root(divMaker.hpointRootContent())),
+  randomPrincipleBody: bodyMaker(divMaker.randomPrincipleRootContent()),
+  randomFingerBody: bodyMaker(divMaker.randomFingerRootContent()),
+  randombaseBody: bodyMaker(divMaker.root()),
 };
 module.exports = body;
