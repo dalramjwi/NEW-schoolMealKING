edd910791ae35506ab6243cd2162f286578b5c83
dalramjwi <dalramjwi@gmail.com>
2024-08-30 18:34:11 +0900

    randompage 출현 확인, 이미지 삽입 필요

--- src/my_module/server_module/route_actions/random_page.js
+++ src/my_module/server_module/route_actions/random_page.js
@@ -1,12 +1,23 @@
+const m = require("../../../module_assemble.js");
+
 /**
  * 랜덤 페이지 처리 (추가 로직 필요)
  * @param {Object} res - 응답 객체
  */
 function randomPage(res) {
-  res.json({
-    success: true,
-    message: "Random page processing not implemented",
-  });
+  // 랜덤으로 선택할 페이지 목록
+  const pages = [
+    "randomPrinciple",
+    "randomFinger",
+    "randomrefrigeator",
+    "randomseasoning",
+  ];
+  // 0부터 pages.length - 1 사이의 정수를 무작위로 생성하여 HTML 페이지를 선택
+  const randomIndex = Math.floor(Math.random() * pages.length);
+  const selectedPage = pages[randomIndex];
+
+  // 선택된 페이지의 HTML을 응답으로 전송
+  res.send(m.componentAssemble[selectedPage]);
 }
 
 module.exports = randomPage;
