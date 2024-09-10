0123e509c10403edc6935481f9b46a1c1caaaa47
dalramjwi <dalramjwi@gmail.com>
2024-09-10 23:14:15 +0900

    서버 라우터화를 위한 코드 리딩

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const m = require("./src/module_assemble.js");
 const database = require("sqlite3").verbose();
 const routeMain = require("./src/my_module/server_module/route_main.js");
 const routeMenu = require("./src/my_module/server_module/route_menu.js");
 const routeCafe = require("./src/my_module/server_module/route_cafe.js");
 const routeCafeData = require("./src/my_module/server_module/route_cafeData.js");
 const routeReturn = require("./src/my_module/server_module/route_return.js");
-const createDb = require("./src/my_module/db_module/createDb.js");
-const insertDb = require("./src/my_module/db_module/insertDb.js");
 const work = require("./database.js");
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 // 데이터베이스 연결
 const db = new database.Database(`./database/school.db`, (err) => {
@@ -16,29 +16,26 @@ const db = new database.Database(`./database/school.db`, (err) => {
     console.log("에러 발생 : ", err);
   }
 });
-
-const app = express();
-const port = process.env.PORT || 3000;
+//db 실행
+work();
 
 // 빈 객체 `result` 정의
 let result = { hpointAll: 0, ypointAll: 0 };
-work();
+//이벤트 상태 저장
+let fingerEventOccurred = false; // 손가락 이벤트 상태 저장
+let principleEventOccurred = false; // 교장 선생님 이벤트 상태 저장
+let refrigeEventOccurred = false; // 냉장고 고장 이벤트 상태 저장
+let seasoningEventOccurred = false; // 조미료 발견 이벤트 상태 저장
 
 app.use("/public", express.static("public"));
 app.use(express.json());
-1;
+
 // 라우터 설정
 app.use("/", routeMain(db));
 app.use("/menu", routeMenu(db));
 app.use("/cafe", routeCafe(db, result));
 app.use("/cafeData", routeCafeData(db));
 app.use("/return", routeReturn(db));
-// 손가락 베임 이벤트 처리 엔드포인트
-let fingerEventOccurred = false; // 손가락 이벤트 상태 저장
-let principleEventOccurred = false; // 교장 선생님 이벤트 상태 저장
-let refrigeEventOccurred = false; // 냉장고 고장 이벤트 상태 저장
-let seasoningEventOccurred = false; // 조미료 발견 이벤트 상태 저장
-
 app.post("/finger", (req, res) => {
   const { event, effect } = req.body;
   console.log("finger 이벤트 요청 수신:", event, effect);
