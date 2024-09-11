948d766fb17ad6fa7f1fdcc795e03ea0ff84b333
dalramjwi <dalramjwi@gmail.com>
2024-09-11 12:45:04 +0900

    초기화 체크

--- src/my_module/server_module/route_restart.js
+++ src/my_module/server_module/route_restart.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { eventState, resetEventState } = require("./eventState");
 module.exports = function (db) {
   const router = express.Router();
 
@@ -26,7 +26,10 @@ module.exports = function (db) {
           });
         }
         console.log("sum 테이블에서 데이터 삭제됨");
-
+        // 상태 값 초기화
+        resetEventState();
+        console.log(resetEventState());
+        console.log(eventState);
         // 모든 작업이 완료되었을 때 성공 응답을 보냅니다.
         res.json({
           success: true,
