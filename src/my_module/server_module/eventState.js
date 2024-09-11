ef4fcbf5216ce70349bb398ebd58480baff9b0ca
dalramjwi <dalramjwi@gmail.com>
2024-09-11 10:57:20 +0900

    event 객체 제작

---
+++ src/my_module/server_module/eventState.js
@@ -0,0 +1,8 @@
+const eventState = {
+  fingerEventOccurred: false,
+  principleEventOccurred: false,
+  refrigeEventOccurred: false,
+  seasoningEventOccurred: false,
+};
+
+module.exports = eventState;
